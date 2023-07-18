b2d809430b615be240f0a75c48ccb441ab38d08c
chidozhou <chidochashegerald@gmail.com>
2023-07-18 11:45:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int x;
-
-	if (n < 0)
-	n = -n;
-
-	x = n % 10;
+	int x = n % 10;
 
 	if (x < 0)
-		x = -x;
+		x *= -1;
 
 	_putchar(x + '0');
 
 	return (x);
+
 }
 
