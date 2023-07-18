d30e20416092acaf05e806d5b647e57a61860b10
chidozhou <chidochashegerald@gmail.com>
2023-07-18 10:38:00 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - sign of a number is printing
+ * @n: the int to check
+ * Return: 1 then prints + if n > zero
+ * 0 and prints 0 if n = zero
+ * -1 and prints - if n < zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	else
+
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
