e28b65fb08b0d0b3d2b8ac4735586847f0e9b1ee
chidozhou <chidochashegerald@gmail.com>
2023-07-20 02:51:54 -0700

    ./0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Checks for the uppercase characters
+ * @c: This is the variable txt
+ * Return: Always 0.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
