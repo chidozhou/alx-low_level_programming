c9ddf142408fcb9add755a76c9cb2c2b075e62b0
chidozhou <chidochashegerald@gmail.com>
2023-07-20 08:45:31 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,16 @@
  */
 void print_number(int n)
 {
-unsigned int num1
+unsigned int num1 = n;
 
 if (n < 0)
 {
 num1 = -n;
 _putchar('-');
 }
-else
-{
-num1 = n;
-}
 
-if (num1 / 10)
-{
+if ((num1 / 10) > 0)
 print_number(num1 / 10);
-}
 
 _putchar((num1 % 10) + '0');
 }
