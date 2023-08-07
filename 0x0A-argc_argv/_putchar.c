be40033a3f309605265ac2677f85315d9dc1a156
chidozhou <chidochashegerald@gmail.com>
2023-08-07 00:19:34 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - Writes the char c to stdout
+ * @c:the character to print
+ *
+ * Return: success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
