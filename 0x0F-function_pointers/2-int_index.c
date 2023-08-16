2cc1b34b2b01aad690f59167c91da64b69a3501f
chidozhou <chidochashegerald@gmail.com>
2023-08-16 10:08:34 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: an array
+ * @size: size of elements in array
+ * @cmp: Pointer to function of 1 of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
+
